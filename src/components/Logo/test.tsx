75b494ea69f15d7c5434d918b43f40b03beeb29b
Phillippe Rocha <phillipperocha@ROTTEIN.localdomain>
2021-02-07 17:02:57 -0300

    Creating the first test and fixing jest module path

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,13 +1,22 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
+  it('should render a white label by default', () => {
+    renderWithTheme(<Logo />)
 
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA',
+    })
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render a black label when color black is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517',
+    })
   })
 })
