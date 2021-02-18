68ba6e04d3e0af6ad10e02ea4078e19b90c4119b
Phillippe Rocha <phillipperocha@ROTTEIN.localdomain>
2021-02-18 20:25:11 -0300

    Creating menu open/close logic using react hooks

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,4 +1,5 @@
-import { screen } from '@testing-library/react'
+// Temos que importar o fireEvent para usá-lo
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Menu from '.'
@@ -13,5 +14,22 @@ describe('<Menu />', () => {
     expect(screen.getByLabelText(/open shopping cart/i))
 
     expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+  })
+
+  it('should handle the open/close mobile menu', () => {
+    renderWithTheme(<Menu />)
+
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
 })
