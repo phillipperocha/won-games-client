e7691950e7b623bfaaea75a297cfa046b10604ba
Phillippe Rocha <phillipperocha@ROTTEIN.localdomain>
2021-02-18 21:09:14 -0300

    Creating styles for full menu

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,4 +1,3 @@
-// Temos que importar o fireEvent para usá-lo
 import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
@@ -19,15 +18,19 @@ describe('<Menu />', () => {
   it('should handle the open/close mobile menu', () => {
     renderWithTheme(<Menu />)
 
+    // Select the nav menu
     const fullMenuElement = screen.getByRole('navigation', { hidden: true })
 
+    // Check if it's hidden
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
 
+    // Click on open menu button and check if it is opened
     fireEvent.click(screen.getByLabelText(/open menu/i))
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
     expect(fullMenuElement).toHaveStyle({ opacity: 1 })
 
+    // Click on close menu button and check if it is closed
     fireEvent.click(screen.getByLabelText(/close menu/i))
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
