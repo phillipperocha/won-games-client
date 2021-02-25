abcdd44b1f06c3328922730cab3d78e234c45bb7
Phillippe Rocha <phillipperocha@gmail.com>
2021-02-25 14:51:05 -0300

    Fixing Heading component to have size and lineColors

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -18,10 +18,11 @@ describe('<Heading />', () => {
     })
   })
 
+  // Como agora temos a lineColor, se não passarmos nada a cor a linha tem que ser rosa
   it('should render a heading with a line on the left side', () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      'border-left': '0.7rem solid #3CD3C1',
+      'border-left': '0.7rem solid #F231A5',
     })
   })
 
@@ -34,5 +35,54 @@ describe('<Heading />', () => {
         modifier: '::after',
       }
     )
+  })
+
+  // Testar o heading com tamanho small
+  it('should render a heading with a small size', () => {
+    // E testaremos o Heading se o font-size dele for de 1.6 rem
+    renderWithTheme(<Heading size="small">Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'font-size': '1.6rem',
+    })
+
+    // E vamos verificar o tamanho da nossa linha junto do heading, que fizemos no &::after
+    // O width tem que estar menor, 3rem
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'width',
+      '3rem',
+      {
+        modifier: '::after',
+      }
+    )
+  })
+
+  // Faremos os testes pra testar a cor primária no line color
+  it('should render a Heading with a primary line color', () => {
+    renderWithTheme(
+      <Heading lineColor="primary" lineLeft lineBottom>
+        Lorem Ipsum
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /lorem ipsum/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #F231A5' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #F231A5', {
+      modifier: '::after',
+    })
+  })
+
+  // E a cor secundária no linecolor
+  it('should render a Heading with a secondary line color', () => {
+    renderWithTheme(
+      <Heading lineColor="secondary" lineLeft lineBottom>
+        Lorem Ipsum
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /lorem ipsum/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #3CD3C1' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #3CD3C1', {
+      modifier: '::after',
+    })
   })
 })
