21259aa63f9d2c42ec927db5af4916098b9eea46
Phillippe Rocha <phillipperocha@gmail.com>
2021-02-28 00:07:24 -0300

    Banner component: test

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -3,6 +3,28 @@ import { renderWithTheme } from 'utils/tests/helpers'
 
 import Banner from '.'
 
+// Vamos criar uma props pro teste para servir pros demais
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death',
+}
+
 describe('<Banner />', () => {
-  it('should render correctly', () => {})
+  it('should render correctly', () => {
+    renderWithTheme(<Banner {...props} />)
+    expect(
+      screen.getByRole('heading', { name: /defy death/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /Play the new CrashLands season/i,
+      })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument()
+  })
 })
