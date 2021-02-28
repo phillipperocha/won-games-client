c7ada85194b01c3c1ae2ec5e5a80cb5ecd953551
Phillippe Rocha <phillipperocha@gmail.com>
2021-02-27 23:50:38 -0300

    Component Banner: Initial structure

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,8 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Banner from '.'
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {})
+})
