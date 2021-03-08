d512183fd426a346255f9c169786532b4755236e
Phillippe Rocha <phillipperocha@gmail.com>
2021-03-08 20:30:10 -0300

    Creating Highlight component basic structure and tests

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,9 +1,15 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Highlight from '.'
+import Highlight, { HighlightProps } from '.'
 
 export default {
   title: 'Highlight',
   component: Highlight,
+  args: {
+    title: 'Red Dead is Back',
+    subtitle: "Come see John's new adventures",
+    buttonLabel: 'Buy now',
+    buttonLink: '/rdr2',
+  },
 } as Meta
 
-export const Default: Story = (args) => <Highlight {...args} />
+export const Default: Story<HighlightProps> = (args) => <Highlight {...args} />
